4ac2d4317f0ecf5f953d5adfb5424178d7733ae9
mdebagui <markdalebagui@gmail.com>
2026-07-19 02:22:26 +0800

    add new project and put image on it

---
+++ latest.update.js
@@ -0,0 +1,6 @@
+// last-updated.js
+window.addEventListener('DOMContentLoaded', () => {
+  document.querySelectorAll('.js-last-updated').forEach(el => {
+    el.textContent = "July 18, 2026";
+  });
+});
